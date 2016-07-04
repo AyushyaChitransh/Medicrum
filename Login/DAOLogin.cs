8786d23de45fbc929c7679e6c45fb36a52ef8f94
Ayushya <ayushyachitransh@gmail.com>
2016-07-04 16:57:17 +0530

    Login Page

---
+++ Login/DAOLogin.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalStoreModule.App_Code.DAO
+{
+    public class DAOLogin
+    {
+        public bool VerifyUser(string email, string password)
+        {
+            ConnectionManager cm = new ConnectionManager();
+            string qry = "SELECT count(*) FROM user where email=@email AND password=@password";
+            if (cm.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(qry, cm.connection);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@password", password);
+                try
+                {
+                    if (cmd.ExecuteNonQuery()>0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Login/Login.aspx.cs
@@ -0,0 +1,24 @@
+﻿using MedicalStoreModule.App_Code.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MedicalStoreModule
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static bool VerifyCredentials(string email, string password)
+        {
+            return (new DAOLogin()).VerifyUser(email, password);
+        }
+    }
+}
